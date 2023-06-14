e20b24fd8d437caa2124c56229aa3a05a033430d
Nikhil Shinde <nikhil@chai.finance>
2023-06-14 17:51:48 +0530

    Add public valid keys

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
 	webpush "github.com/SherClockHolmes/webpush-go"
 )
 
+// const (
+// 	subscription    = "mailto: <nikhil@chai.finance>"
+// 	vapidPublicKey  = "BJkwr8YFq0MvT7PxYFrsapQkS82fSHcGDM5t7aGBF4FBlRCM8Pjy1APatWUjrvx-ICk4io54ks7GN78jPqyhZYo"
+// 	vapidPrivateKey = "muV0vo0ls3pC08z8NGdp3YxbleGZLDa3lpt3tCFBaE4"
+// )
+
 const (
-	subscription    = "mailto: <nikhil@chai.finance>"
+	subscription    = ``
 	vapidPublicKey  = "BJkwr8YFq0MvT7PxYFrsapQkS82fSHcGDM5t7aGBF4FBlRCM8Pjy1APatWUjrvx-ICk4io54ks7GN78jPqyhZYo"
 	vapidPrivateKey = "muV0vo0ls3pC08z8NGdp3YxbleGZLDa3lpt3tCFBaE4"
 )
@@ -19,12 +26,13 @@ func main() {
 
 	// Send Notification
 	resp, err := webpush.SendNotification([]byte("Test"), s, &webpush.Options{
-		Subscriber:      subscription,
-		VAPIDPublicKey:  vapidPublicKey,
+		Subscriber:      "nikhil@chai.finance",
 		VAPIDPrivateKey: vapidPrivateKey,
+		VAPIDPublicKey:  vapidPublicKey,
 		TTL:             30,
 	})
 	if err != nil {
+		fmt.Println("err :: ", err)
 		// TODO: Handle error
 	}
 	defer resp.Body.Close()
